486184cdd22e3ace10990e97f2b6d0ef5ddffad8
RomanKulik <roman.ser.kulik@gmail.com>
2022-02-18 19:13:37 +0200

    task02_Hello_world

---
+++ 01.NIX-Beginner-Level-1/src/task02_Hello_world/T02_HelloWorld.java
@@ -0,0 +1,14 @@
+package task02_Hello_world;
+
+/**
+ * Hello world
+ * Создайте класс, который выводит на экран надпись "Hello world"
+ *
+ * Класс создан, запускается из консоли, при этом выводит "Hello world" на экран
+ */
+
+public class T02_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
